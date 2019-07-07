10aed8e7207f3e7a126faa1ac0491801056a2b26
Forb <nikolay.mashkov@gmail.com>
2019-07-07 12:46:20 +0300

    рефактор в point

--- sandbox/src/main/java/ru/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/sandbox/MyFirstProgram.java
@@ -12,13 +12,10 @@ public class MyFirstProgram {
     Rectangle r= new Rectangle(4,5);
     System.out.println("Площадь прямоугольника со сторонами "+ r.a + " и "+ r.b +" = " +r.area());
 
-    Point p1=new Point();
-    Point p2 = new Point();
-    p1.x=5;
-    p1.y=6;
-    p2.x=7;
-    p2.y=8;
-    System.out.println("Расстояние между двумя точками  "+ p1.x + ", "+ p1.y +" и "+ p2.x + ", "+p2.y + " = " +distance(p1,p2));
+    Point p1=new Point(5,6);
+    Point p2 = new Point(7,8);
+
+    System.out.println("Расстояние между двумя точками  "+ p1.x + ", "+ p1.y +" и "+ p2.x + ", "+p2.y + " = " +p1.distance(p2));
 
   }
   public static void hello(String somebody){
@@ -26,9 +23,9 @@ public class MyFirstProgram {
 
   }
 
-  public static double distance(Point p1, Point p2){
-    return  Math.sqrt((p2.x-p1.x)*(p2.x-p1.x)+(p2.y-p1.y)*(p2.y-p1.y));
-  }
+//  public static double distance(Point p1, Point p2){
+//    return  Math.sqrt((p2.x-p1.x)*(p2.x-p1.x)+(p2.y-p1.y)*(p2.y-p1.y));
+//  }
 
 
 
