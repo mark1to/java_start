7215a60c797cce980700562efdba3283e1a3ec59
Forb <nikolay.mashkov@gmail.com>
2019-07-07 12:36:06 +0300

    2ое задание

--- sandbox/src/main/java/ru/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/sandbox/MyFirstProgram.java
@@ -4,12 +4,21 @@ public class MyFirstProgram {
 
   public static void main(String[] args) {
     hello("Kolya");
-    double l=5;
-    System.out.println("Площадь квадрата со стороной "+ l + " = " +area(l));
 
-    double a=4;
-    double b=5;
-    System.out.println("Площадь прямоугольника со сторонами "+ a + " и "+ b +" = " +area(a,b));
+
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной "+ s.l + " = " + s.area());
+
+    Rectangle r= new Rectangle(4,5);
+    System.out.println("Площадь прямоугольника со сторонами "+ r.a + " и "+ r.b +" = " +r.area());
+
+    Point p1=new Point();
+    Point p2 = new Point();
+    p1.x=5;
+    p1.y=6;
+    p2.x=7;
+    p2.y=8;
+    System.out.println("Расстояние между двумя точками  "+ p1.x + ", "+ p1.y +" и "+ p2.x + ", "+p2.y + " = " +distance(p1,p2));
 
   }
   public static void hello(String somebody){
@@ -17,11 +26,10 @@ public class MyFirstProgram {
 
   }
 
-  public static double area(double len){
-    return len*len;
+  public static double distance(Point p1, Point p2){
+    return  Math.sqrt((p2.x-p1.x)*(p2.x-p1.x)+(p2.y-p1.y)*(p2.y-p1.y));
   }
 
-  public static double area(double a,double b){
-    return a*b;
-  }
+
+
 }
